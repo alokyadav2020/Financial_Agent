56c04d0ac084e51cbaa5ca7e0d0b1bd0f6ff3491
unknown <alokranjan.ucer@gmail.com>
2025-06-29 17:03:28 +0530

    created new connection string

--- src/db/connection.py
+++ src/db/connection.py
@@ -2,19 +2,27 @@
 from contextlib import contextmanager
 import pyodbc
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 def get_connection():
    
-    conn_str = f'''
-        DRIVER={os.getenv("driver")};
-        SERVER={os.getenv("server")};
-        DATABASE={os.getenv("database")};
-        UID={os.getenv("username")};
-        PWD={os.getenv("password_")};
-        Encrypt=yes;
-        TrustServerCertificate=no;
-        Connection Timeout=30;
-    '''
+    # conn_str = f'''
+    #     DRIVER={os.getenv("driver")};
+    #     SERVER={os.getenv("server")};
+    #     DATABASE={os.getenv("database")};
+    #     UID={os.getenv("username")};
+    #     PWD={os.getenv("password_")};
+    #     Encrypt=yes;
+    #     TrustServerCertificate=no;
+    #     Connection Timeout=30;
+    # '''
+    conn_str = os.getenv("AzureSQLConnectionString")
+
+    if not conn_str:
+        raise ValueError("Connection string not found in environment variables!")
+
+    # Optional: fix driver string format if needed
+    conn_str = conn_str.replace("Driver={ODBC Driver 18 for SQL Server}", "DRIVER={ODBC Driver 18 for SQL Server}")
     return pyodbc.connect(conn_str)
 
 
